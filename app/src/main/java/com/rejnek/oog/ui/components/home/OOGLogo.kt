0f01c4aae1a3198e1759975bc25b5cf609b2916a
Lukas R <rejneluk@fel.cvut.cz>
2025-10-06 17:36:57 +0200

    add Questopix logo, rename app

--- app/src/main/java/com/rejnek/oog/ui/components/home/OOGLogo.kt
+++ app/src/main/java/com/rejnek/oog/ui/components/home/OOGLogo.kt
@@ -3,10 +3,12 @@ package com.rejnek.oog.ui.components.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.rejnek.oog.R
 
 @Composable
@@ -14,10 +16,11 @@ fun OOGLogo(
     modifier: Modifier = Modifier,
 ){
     Image (
-        painter = painterResource(id = R.drawable.ooglogo),
+        painter = painterResource(id = R.drawable.questopix_logo),
         contentDescription = stringResource(R.string.cd_game_icon),
         modifier = modifier
-            .fillMaxWidth()
+            .fillMaxWidth(0.8f)
+            .padding(top = 25.dp)
             .aspectRatio(1f),
     )
 }
